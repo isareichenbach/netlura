c27cf5351a7deeac46e94f50960f6a260fcb1c11
isareichenbach <isadora.reichembach@gmail.com>
2026-03-30 18:00:16 -0300

    Add files via upload

---
+++ catalogo/js/data.js
@@ -0,0 +1,41 @@
+export const categories = [
+    {
+        title: "Studio Ghibli",
+        items: [
+            {
+                img: "https://ingresso-a.akamaihd.net/prd/img/movie/ghibli-fest-2025-meu-amigo-totoro/e41339ce-ce11-433a-8bb2-67863cdbe6bd.webp", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=92a7Hj0ijLs&pp=ygUOdHJhaWxlciB0b3Rvcm8%3D"
+            },
+            {
+                img: "https://m.media-amazon.com/images/M/MV5BYmZmMmM4OTYtMDkyNi00ZDI5LThiODItNzhlZGI3ZDJmZDZiXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=ByXuk9QqQkk&pp=ugMGCgJwdBABugUEEgJwdMoFG3RyYWlsZXIgYSB2aWFnZW0gZGUgY2hpaGlyb9gHAQ%3D%3D"
+            },
+            {
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_NIL8O092t5LiMPY0FQvWbJvGvMZsQOkRIg&s", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=cAh5-8jXTpE&pp=ygUcc2VydmnDp28gZGUgZW50cmVnYXMgZGEga2lradIHCQnZCgGHKiGM7w%3D%3D"
+            },
+            {
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRlISDDbAJLhYOZHsBI5R7XjxEVu1A_7_-Xw&s", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=tr4azS1Kxp8&pp=ygUabyBjb250byBkYSBwcmluY2VzYSBrYWd1eWE%3D"
+            },
+        ]
+    },
+    {
+        title: "Séries",
+        items: [
+            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "#" },
+            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "#" },
+            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novo episódio", badgeColor: "red", youtube: "#" },
+            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novidade", badgeColor: "red", youtube: "#" },
+        ]
+    },
+    {
+        title: "Para maratonar",
+        items: [
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novidade", badgeColor: "red", youtube: "#" },
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "#" },
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "#" },
+        ]
+    }
+];
